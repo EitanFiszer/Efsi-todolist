113619595197bd3cff51802ed2e70f7c90a3768e
EITAN DANIEL <46028383@ort>
2022-05-23 10:45:55 -0300

    hay que tachar

--- codigo.js
+++ codigo.js
@@ -1,9 +1,14 @@
 let tareas=[]
 
 let crearTarea = (nombre) =>{
-    tareas.push(nombre)
+    nombre = document.getElementById(nombre).value;
     console.log(nombre)
-    };
+    tareas.push(nombre);
+    let lista=document.getElementById("tareas")
+    lista.innerHTML += `<li style="list-style-type: none"><input type="checkbox"></input>
+                        <label class="tachado">${nombre}</label>
+                        </li>`                         
+}
 
 
 
